5e9ce65dfd2c95931e2bd43c22ddac75c4d4bd89
monish <monishbarse9@gmail.com>
2024-04-12 21:22:34 +0530

    routs changed

--- routes/commonRoutes.js
+++ routes/commonRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import SizesController from "../controllers/sizesController.js";
+import OrganizationsController from "../controllers/organizationsController.js";
 
 const router = express.Router();
 
 // Public Routes
-//sizes
-router.post("/savesize",SizesController.saveSize);
-router.get("/getallsizes",SizesController.getAllSizes);
+//organization
+router.post("/saveorganizations ",OrganizationsController.saveOrganization);
+router.get("/getallorganizations",OrganizationsController.getAllOrganizations);
 
 export default router;
