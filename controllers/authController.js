80db0db91538d0dfe59383e6397373f649fbe708
monish <monishbarse9@gmail.com>
2024-08-01 13:11:28 +0530

    Invalid Data Handaled in Get Profile

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,68 +17,87 @@ const expirationTime =
 class AuthController {
   static getUser = async (req, res) => {
     const { external_user_id } = req.body;
-    const decrypted_external_user_id = decryptJSON(external_user_id)
-    try {
-      let user;
-      let userProfiles;
-      let noOfUsers = await getAllUsers();
-      if (noOfUsers?.length <= 0) {
-        // first setup functions for static tables
-        await InitiatSetupWizard();
-        // create user
-        user = await CreateUser({ external_user_id:decrypted_external_user_id });
-        // create admin
-        await SystemUsersFunctions.CreateSystemAdmin({
-          user_id: user?._id,
-        });
-
-      } else {
-        user = await db.UsersModal.findOne({ external_user_id:decrypted_external_user_id });
-        if (!user) {
-          user = await CreateUser({ external_user_id:decrypted_external_user_id });
-          await SystemUsersFunctions.CreateSystemGuest({
+    await decryptJSON(external_user_id)
+      .then(async (decrypted_external_user_id) => {
+        try {
+          let user;
+          let userProfiles;
+          let noOfUsers = await getAllUsers();
+          if (noOfUsers?.length <= 0) {
+            // first setup functions for static tables
+            await InitiatSetupWizard();
+            // create user
+            user = await CreateUser({
+              external_user_id: decrypted_external_user_id,
+            });
+            // create admin
+            await SystemUsersFunctions.CreateSystemAdmin({
+              user_id: user?._id,
+            });
+          } else {
+            user = await db.UsersModal.findOne({
+              external_user_id: decrypted_external_user_id,
+            });
+            if (!user) {
+              user = await CreateUser({
+                external_user_id: decrypted_external_user_id,
+              });
+              await SystemUsersFunctions.CreateSystemGuest({
+                user_id: user._id,
+              });
+            }
+          }
+          // fetch profiles
+          let systemProfiles = await SystemUsersFunctions.getSystemUsersList({
             user_id: user._id,
           });
+          let organizationProfiles =
+            await OrganizationUsersFunctions.getOrganizationUsersList({
+              user_id: user._id,
+            });
+          let clientProfiles = await ClientUsersFunctions.getClientUsersList({
+            user_id: user._id,
+          });
+          userProfiles = {
+            systemProfiles,
+            organizationProfiles,
+            clientProfiles,
+          };
+          const token = jwt.sign(
+            { data: { user, userProfiles } },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: expirationTime }
+          );
+          sendResult(res, token, "Logged in");
+        } catch (error) {
+          sendError(res, error.message, "Loggin Failed");
         }
-      }
-      // fetch profiles
-      let systemProfiles = await SystemUsersFunctions.getSystemUsersList({user_id: user._id})
-      let organizationProfiles = await OrganizationUsersFunctions.getOrganizationUsersList({user_id: user._id})
-      let clientProfiles = await ClientUsersFunctions.getClientUsersList({user_id: user._id})
-      userProfiles = {
-        systemProfiles,
-        organizationProfiles,
-        clientProfiles
-      }
-      const token = jwt.sign(
-        { data: {user,userProfiles} },
-        process.env.JWT_SECRET_KEY,
-        { expiresIn: expirationTime }
-      );
-      sendResult(res, token, "Logged in");
-    } catch (error) {
-      sendError(res, error.message, "Loggin Failed");
-    }
+      })
+      .catch((error) => {
+        sendError(res, error, "Invalid Data");
+      });
   };
   static getProfileConfig = async (req, res) => {
     const { external_role_id } = req.body;
-    const decrypted_external_role_id = decryptJSON(external_role_id)
+    const decrypted_external_role_id = decryptJSON(external_role_id);
     try {
-      let access = await UserAccessFunctions.FindUserAccessList({role_id: decrypted_external_role_id})
+      let access = await UserAccessFunctions.FindUserAccessList({
+        role_id: decrypted_external_role_id,
+      });
       sendResult(res, access, "Fetch Config");
     } catch (error) {
       sendError(res, error.message, "Fetch Config Failed");
     }
   };
 
-  static InitiatSetupWizard = async (req, res) =>{
+  static InitiatSetupWizard = async (req, res) => {
     try {
       await InitiatSetupWizard();
       sendResult(res, {}, "Imported data Successfully");
     } catch (error) {
-            sendError(res, error.message, "Failed");
-    } 
-  }
+      sendError(res, error.message, "Failed");
+    }
+  };
 }
 
 export default AuthController;
--- utils/encryptdecrypt.js
+++ utils/encryptdecrypt.js
@@ -4,17 +4,31 @@ dotenv.config();
 
 // Encrypt JSON object
 export function encryptJSON(obj) {
-  let key = process.env.JWT_SECRET_KEY;
-  const jsonString = JSON.stringify(obj);
-  const ciphertext = CryptoJS.AES.encrypt(jsonString, key).toString();
-  return ciphertext;
+  return new Promise((resolve, reject) => {
+    try {
+      let key = process.env.JWT_SECRET_KEY;
+      const jsonString = JSON.stringify(obj);
+      const ciphertext = CryptoJS.AES.encrypt(jsonString, key).toString();
+      resolve(ciphertext);
+    } catch (error) {
+      console.log(error);
+      reject("Encryption Failed");
+    }
+  });
 }
 
 // Decrypt JSON object
 export function decryptJSON(ciphertext) {
-  let key = process.env.JWT_SECRET_KEY;
-  const bytes = CryptoJS.AES.decrypt(ciphertext, key);
-  const decryptedString = bytes.toString(CryptoJS.enc.Utf8);
-  const decryptedObj = JSON.parse(decryptedString);
-  return decryptedObj;
+  return new Promise((resolve, reject) => {
+    try {
+      let key = process.env.JWT_SECRET_KEY;
+      const bytes = CryptoJS.AES.decrypt(ciphertext, key);
+      const decryptedString = bytes.toString(CryptoJS.enc.Utf8);
+      const decryptedObj = JSON.parse(decryptedString);
+      resolve(decryptedObj);
+    } catch (error) {
+      console.log(error);
+      reject("Decryption Failed");
+    }
+  });
 }
